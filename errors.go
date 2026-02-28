ac75c76669e9079acb3dd43f88f5a4167b96e223
Patricio Whittingslow <graded.sp@gmail.com>
2026-02-28 16:00:43 -0300

    format errors.go

--- errors.go
+++ errors.go
@@ -4,22 +4,22 @@ type errGeneric uint8
 
 // Generic errors common to internet functioning.
 const (
-	_                  errGeneric = iota // non-initialized err
-	ErrBug                               // lneto-bug(use build tag "debugheaplog")
-	ErrPacketDrop                        // packet dropped
-	ErrBadCRC                            // incorrect checksum
-	ErrZeroSource                        // zero source(port/addr)
-	ErrZeroDestination                   // zero destination(port/addr)
-	ErrShortBuffer                       // short buffer
-	ErrBufferFull                        // buffer full
-	ErrInvalidAddr                       // invalid address
-	ErrUnsupported                       // unsupported
-	ErrMismatch                          // mismatch
-	ErrMismatchLen                       // mismatched length
-	ErrInvalidConfig                     // invalid configuration
-	ErrInvalidField                      // invalid field
-	ErrInvalidLengthField                // invalid length field
-	ErrExhausted                         // resource exhausted
+	_                     errGeneric = iota // non-initialized err
+	ErrBug                                  // lneto-bug(use build tag "debugheaplog")
+	ErrPacketDrop                           // packet dropped
+	ErrBadCRC                               // incorrect checksum
+	ErrZeroSource                           // zero source(port/addr)
+	ErrZeroDestination                      // zero destination(port/addr)
+	ErrShortBuffer                          // short buffer
+	ErrBufferFull                           // buffer full
+	ErrInvalidAddr                          // invalid address
+	ErrUnsupported                          // unsupported
+	ErrMismatch                             // mismatch
+	ErrMismatchLen                          // mismatched length
+	ErrInvalidConfig                        // invalid configuration
+	ErrInvalidField                         // invalid field
+	ErrInvalidLengthField                   // invalid length field
+	ErrExhausted                            // resource exhausted
 )
 
 func (err errGeneric) Error() string {
